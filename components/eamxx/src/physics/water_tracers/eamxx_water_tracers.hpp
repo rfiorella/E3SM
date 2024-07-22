5f5486aa5ddac9c0752f70296d79b3f08bbb184e
Rich Fiorella <rfiorella@lanl.gov>
2024-07-21 19:50:08 -0600

    wip - definition of WaterIsotopologue and WaterTracer structs

--- components/eamxx/src/physics/water_tracers/eamxx_water_tracers.hpp
+++ components/eamxx/src/physics/water_tracers/eamxx_water_tracers.hpp
@@ -23,13 +23,22 @@ WaterTracers::WaterTracers (const ekat::Comm& comm, const ekat::ParameterList& p
     bool wtrc_alpha_kinetic;
     bool wtrc_use_ice_supsat;
 
-    Real wtrc_fixed_alpha = 1.
-    Real wtrc_fixed_rstd  = 1.
-    Real wtrc_qchkmin = 1e-14
-    Real wtrc_qmin    = 1e-18
+    static const Real wtrc_fixed_alpha = 1.; // default standard fractionation factor
+    static const Real wtrc_fixed_rstd  = 1.; // default standard water isotope ratio (1 chosen for numerics)
+    static const Real wtrc_qchkmin = 1e-14;
+    static const Real wtrc_qmin    = 1e-18;
 
-    int wtrc_ncnst = 0 // number of water tracer sets
+    int wtrc_ncnst = 0; // number of water tracer sets
+    int wtrc_niter = 10; // number of iterations when applying process rates
 
+
+    // Tunable parameters related to water isotopic fraciontation schemes.
+    static const Real dkfac   = 0.58; // diffusive evaporative kinetic power law exponent (Stewart 1975)
+    static const Real ssatmx  = 2.0;  // maximum value of supersaturation parameters
+    static const Real fsata   = 1.;   // calculate supersaturation parameter s = fsata + fsatb
+    static const Real fsatb   = -0.002; // supersaturation parameter s = fsata + fsatb 
+
+    static const double hlhto = 3.888e8; // seconds
 
 }
 
